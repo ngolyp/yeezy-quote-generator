0dae637fe2a09124b3d3bc002045cb2115e094ed
Peter Ngo-Ly <ngoly@sheridancollege.ca>
2019-10-14 14:46:36 -0400

    Updates

---
+++ precache-manifest.5873287640c2899ca8441712cc0b97ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07faf5d4cecc96be56c1ac25fb29496e",
+    "url": "/yeezy-quote-generator/index.html"
+  },
+  {
+    "revision": "f63b190555b393a33fe4",
+    "url": "/yeezy-quote-generator/static/css/main.bf84667f.chunk.css"
+  },
+  {
+    "revision": "ca8ac525fb9076a773fa",
+    "url": "/yeezy-quote-generator/static/js/2.5a89270a.chunk.js"
+  },
+  {
+    "revision": "f63b190555b393a33fe4",
+    "url": "/yeezy-quote-generator/static/js/main.94dbfe2d.chunk.js"
+  },
+  {
+    "revision": "258eb5fc029206865c7b",
+    "url": "/yeezy-quote-generator/static/js/runtime-main.93216320.js"
+  }
+]);
